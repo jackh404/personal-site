41aa72030d971bf17eeaafacc5159b860f5c131c
Jack Henza <jackhenza@gmail.com>
2023-09-25 14:10:16 -0400

    create hover-logo effect for java

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-
+const jvLoBg = document.getElementById("jvLoBg");
+const java = document.getElementById("java");
+java.addEventListener("pointerenter",function(){
+    jvLoBg.style.opacity = "100%";
+})
+java.addEventListener("pointerleave",function(){
+    jvLoBg.style.opacity = "00%";
+})
 //\\Test code//\\
 /*const h2 = document.createElement("h2");
 h2.textContent = "JavaScript!";
